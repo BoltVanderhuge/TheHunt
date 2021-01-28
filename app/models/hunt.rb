daea2ceefb9400d8400c7b74f1a88543540d6c55
john_wisneski <john.j.wisneski@gmail.com>
2021-01-28 09:54:00 -0500

    error_messages

--- app/models/hunt.rb
+++ app/models/hunt.rb
@@ -2,5 +2,8 @@ class Hunt < ApplicationRecord
     has_many :hunt_progressions
     has_many :users, through: :hunt_progressions
 
+    def error_messages
+        messages = ["Are you having a laugh?","What are you daft", "You have chosen poorly", ""]
+    end
    
 end
